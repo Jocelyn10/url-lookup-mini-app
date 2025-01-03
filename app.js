b5ebe31ce349eef59b1815e61f7cc35b55b775c1
Jocelyn10 <jocelynnsa@gmail.com>
2025-01-03 08:56:40 +0100

    add main request

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 App({
-  BASE_URL: 'https://api.fxfeed.io/v1',
-  API_KEY: 'fxf_ZJ1H3jnImnwvdOtV8RY5',
+  BASE_URL: 'https://api.api-ninjas.com/v1',
+  API_KEY: 'mpc6RHiIEiGKwt10nThRwA==rmkekTqZaqL4C5yT',
   
   onLaunch(options) {},
   onShow(options) {},
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,28 @@
 Page({
   handleURLLookup(){
-    
+    const {
+      BASE_URL,
+      API_KEY
+    } = getApp()
+
+    const url = 'google.com'
+
+    const URL = `${BASE_URL}/urllookup?url=${url}`
+
+    my.request({
+      url: URL,
+      method: 'GET',
+      headers: {
+        'X-Api-Key': API_KEY
+      },
+      contentType: 'application/json',
+
+      success: function(response){
+        console.log('Response : ', response);
+      },
+      fail: function(response){
+        console.log('Echec de la requête');
+      }
+    });
   }
 });
